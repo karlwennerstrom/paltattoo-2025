bbfd75493385508a0f19c4edd7522430eb385676
Karl Wennerstrom <karlwennerstrom@gmail.com>
2025-07-24 15:23:52 -0400

    Remove ignored files

--- backend/seeds/create-user.sql
+++
@@ -1,4 +0,0 @@
--- Create a new MySQL user for the application
-CREATE USER IF NOT EXISTS 'tattooconnect'@'localhost' IDENTIFIED BY 'Dracula241988.';
-GRANT ALL PRIVILEGES ON tattoo_connect.* TO 'tattooconnect'@'localhost';
-FLUSH PRIVILEGES;
